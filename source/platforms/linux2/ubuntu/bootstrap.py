9f12fb953bf48acc9999dcf51effcb44bc68ec4d
Lisa Glendenning <lisaglendenning@seagull.konsume.org>
2010-06-30 20:15:05 -0700

    bug fix

--- source/platforms/linux2/ubuntu/bootstrap.py
+++ source/platforms/linux2/ubuntu/bootstrap.py
@@ -27,7 +27,7 @@ def get_version(argv):
     child = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     outdata, errdata = child.communicate()
     version = outdata.split()[1]
-    version = version.split('.')
+    version = tuple(version.split('.'))
     return version
 
 def install_repository(argv, version):
