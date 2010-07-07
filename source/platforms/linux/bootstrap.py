168b28796ffc8992d5ef6a4e61ee0b3eaf2402b9
Lisa Glendenning <lisaglendenning@seagull.konsume.org>
2010-07-06 22:35:03 -0700

    fix

--- source/platforms/linux/bootstrap.py
+++ source/platforms/linux/bootstrap.py
@@ -6,7 +6,7 @@ def main(argv, system):
     # Detect what distribution we're running
     dist = platform.dist()[0].lower()
     try:
-        package = __name__.rsplit('.')[0]
+        package = __name__.rsplit('.', 1)[0]
         mod = __import__('.'.join([package, dist, 'bootstrap']), 
                          globals(), locals(), 'bootstrap')
         mod.main(argv, system, dist)
