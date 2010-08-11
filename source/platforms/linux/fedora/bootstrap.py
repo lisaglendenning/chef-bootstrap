d7614706e9ad53f8f331053c7c81f491c78fb396
Lisa Glendenning <lglenden@cs.washington.edu>
2010-08-11 00:04:47 -0700

    fix

--- source/platforms/linux/fedora/bootstrap.py
+++ source/platforms/linux/fedora/bootstrap.py
@@ -6,6 +6,6 @@ from platforms.linux.redhat.common import *
 MIN_VERSION = ('8')
 
 
-def main(*args):
+def main(opts, args):
     check_version(opts.dist, MIN_VERSION)
     gem_install_chef(opts, args)
