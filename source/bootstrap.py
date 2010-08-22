3d8a202550d9f62b9cdddbb0f4169be52add307f
Lisa Glendenning <lglenden@cs.washington.edu>
2010-08-22 09:28:19 -0700

    minor

--- source/bootstrap.py
+++ source/bootstrap.py
@@ -8,32 +8,12 @@ import sys, os.path, optparse
 import util
 
 
-def configure(argv):
-    r"""Parses command-line arguments"""
-    parser = optparse.OptionParser()
-    parser.add_option("-o", "--os", dest="os", metavar="OS",
-                      help="specify one of [linux] to override detection")
-    parser.add_option("-d", "--dist", dest="dist", metavar="DIST",
-                      help="specify os-specific distribution")
-    parser.add_option("-s", "--server", dest="server", action="store_true",
-                      default=False, help="install the Chef server")
-    parser.add_option("-w", "--webui", dest="webui", action="store_true",
-                      default=False, help="install the Chef server webui")
-    parser.add_option("-u", "--url", dest="url", metavar="URL",
-                      help="specify the Chef server url")
-    parser.add_option("-c", "--name", dest="name", metavar="NAME",
-                      help="specify the Chef client name")
-    parser.add_option("-v", "--validation", dest="validation", metavar="FILE",
-                      help="use as the Chef server validation key")
-    return parser.parse_args()
-
-
 def main(argv):
     
     # add root package to PYTHONPATH
     sys.path.insert(0, os.path.dirname(__file__))
     
-    opts, args = configure(argv)
+    opts, args = util.configure(argv)
     if not opts.url:
         opts.url = "http://127.0.0.1:4000"
     if not opts.os:   
--- source/util.py
+++ source/util.py
@@ -1,6 +1,29 @@
 r"""General utility functions""" 
 
-import subprocess, platform
+import subprocess, optparse
+
+platform
+
+
+def configure(argv):
+    r"""Parses command-line arguments"""
+    parser = optparse.OptionParser()
+    parser.add_option("-o", "--os", dest="os", metavar="OS",
+                      help="specify one of [linux] to override detection")
+    parser.add_option("-d", "--dist", dest="dist", metavar="DIST",
+                      help="specify os-specific distribution")
+    parser.add_option("-s", "--server", dest="server", action="store_true",
+                      default=False, help="install the Chef server")
+    parser.add_option("-w", "--webui", dest="webui", action="store_true",
+                      default=False, help="install the Chef server webui")
+    parser.add_option("-u", "--url", dest="url", metavar="URL",
+                      help="specify the Chef server url")
+    parser.add_option("-c", "--name", dest="name", metavar="NAME",
+                      help="specify the Chef client name")
+    parser.add_option("-v", "--validation", dest="validation", metavar="FILE",
+                      help="use as the Chef server validation key")
+    return parser.parse_args(argv)
+
 
 def execute(args, input=None, **kwargs):
     r"""Execute a command"""
